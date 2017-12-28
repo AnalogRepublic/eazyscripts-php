7b835f91a71e41b1ef7271bad9a3edc11b122f00
Craig Childs <childscraig17@gmail.com>
2017-12-28 15:31:41 +0000

    Hook in prescriber & specialty endpoints & tests

--- src/EazyScripts.php
+++ src/EazyScripts.php
@@ -40,6 +40,19 @@ class EazyScripts
      */
     private $token;
 
+    const USER_LEVEL_DOCTOR = 2;
+    const USER_LEVEL_PATIENT = 3;
+
+    const GENDER_UNKNOWN = 0;
+    const GENDER_MALE = 1;
+    const GENDER_FEMALE = 2;
+
+    const TYPE_HOME = 1;
+    const TYPE_WORK = 2;
+    const TYPE_FAX = 3;
+
+    const PLATFORM_SERVER = "SERVER";
+
     /**
      * Create our EazyScripts API handler with the required
      * API credentials.
@@ -110,7 +123,11 @@ class EazyScripts
      */
     public function addPatient($body)
     {
-        $body = Body::json($body);
+        $default = [
+            "Level" => self::USER_LEVEL_PATIENT,
+        ];
+
+        $body = Body::json(array_merge($default, $body));
 
         $request = new Request("/users", Request::DEFAULT_HEADERS, $body);
 
@@ -131,6 +148,103 @@ class EazyScripts
         $body = Body::json($body);
 
         $request = new Request(sprintf("/patients/%s/info", $id), Request::DEFAULT_HEADERS, $body);
+
+        $request->withAuthorization($this->getToken(), true);
+
+        return $request->post();
+    }
+
+    /**
+     * Get all of the prescriber specialties
+     * 
+     * @return EazyScripts\Http\Response
+     */
+    public function getPrescriberSpecialties()
+    {
+        $request = new Request("/prescribers/specialties");
+
+        $request->withAuthorization($this->getToken(), true);
+
+        return $request->get();
+    }
+
+    /**
+     * Get all of the prescriber specialty 
+     * qualifier types.
+     * 
+     * @return EazyScripts\Http\Response
+     */
+    public function getPrescriberSpecialtyQualifiers()
+    {
+        $request = new Request("/prescribers/specialty-qualifiers");
+
+        $request->withAuthorization($this->getToken(), true);
+
+        return $request->get();
+    }
+
+    /**
+     * Get all prescribers.
+     * 
+     * @return EazyScripts\Http\Response
+     */
+    public function getPrescribers()
+    {
+        $request = new Request("/prescribers");
+
+        $request->withAuthorization($this->getToken(), true);
+
+        return $request->get();
+    }
+
+    /**
+     * Get a single prescriber.
+     * 
+     * @param  string $id
+     * @return EazyScripts\Http\Response
+     */
+    public function getPrescriber($id)
+    {
+        $request = new Request(sprintf("/prescribers/%s/info", $id));
+
+        $request->withAuthorization($this->getToken(), true);
+
+        return $request->get();
+    }
+
+    /**
+     * Create a new prescriber.
+     * 
+     * @param array $body
+     * @return EazyScripts\Http\Response
+     */
+    public function addPrescriber($body)
+    {   
+        $default = [
+            "Level" => self::USER_LEVEL_DOCTOR,
+        ];
+
+        $body = Body::json(array_merge($default, $body));
+
+        $request = new Request("/users", Request::DEFAULT_HEADERS, $body);
+
+        $request->withAuthorization($this->getToken(), true);
+
+        return $request->put();
+    }
+
+    /**
+     * Updated a single prescriber.
+     * 
+     * @param  string $id
+     * @param  array $body
+     * @return EazyScripts\Http\Response
+     */
+    public function updatePrescriber($id, $body)
+    {
+        $body = Body::json($body);
+
+        $request = new Request(sprintf("/prescribers/%s/info", $id), Request::DEFAULT_HEADERS, $body);
 
         $request->withAuthorization($this->getToken(), true);
 
--- tests/EazyScriptsTest.php
+++ tests/EazyScriptsTest.php
@@ -15,6 +15,8 @@ final class EazyScriptsTest extends TestCase
     protected static $token;
     protected static $patient_id;
     protected static $prescriber_id;
+    protected static $specialty_id;
+    protected static $qualifier_id;
 
     public function setUp()
     {
@@ -48,14 +50,14 @@ final class EazyScriptsTest extends TestCase
         $response = $api->authenticate([
             'Email'        => getenv('EAZYSCRIPTS_EMAIL'),
             'Password'     => getenv('EAZYSCRIPTS_PASSWORD'),
-            'PlatformType' => 'SERVER',
-            'Subdomain'    => getenv('EAZYSCRIPTS_SUBDOMAIN')
+            'Subdomain'    => getenv('EAZYSCRIPTS_SUBDOMAIN'),
+            'PlatformType' => EazyScripts::PLATFORM_SERVER,
         ]);
 
         self::$token = $response->getToken();
 
-        $this->assertObjectNotHasAttribute('error', $response->getBody());
-        $this->assertObjectNotHasAttribute('errors', $response->getBody());
+        $this->assertObjectNotHasAttribute('error', (object)$response->getBody());
+        $this->assertObjectNotHasAttribute('errors', (object)$response->getBody());
         $this->assertNotFalse(self::$token);
     }
 
@@ -70,13 +72,12 @@ final class EazyScriptsTest extends TestCase
         $api->setToken(self::$token);
 
         $response = $api->addPatient([
-            "Level"       => 3,
             "FirstName"   => "Testing",
             "LastName"    => "Patient",
             "Email"       => time() . "testing+patient@testemail.com",
             "Password"    => "pa55word",
             "DateOfBirth" => "1970-2-1",
-            "Gender"      => 1,
+            "Gender"      => EazyScripts::GENDER_FEMALE,
             "Patient"     => [
                 "HomeAddress" => [
                     "Address1" => "123 Test Road",
@@ -84,7 +85,7 @@ final class EazyScriptsTest extends TestCase
                     "State"    => "CA",
                     "Country"  => "USA",
                     "Zip"      => "60654",
-                    "Type"     => 1,
+                    "Type"     => EazyScripts::TYPE_HOME,
                 ],
                 "WorkAddress" => [
                     "Address1" => "123 Test Road",
@@ -92,23 +93,23 @@ final class EazyScriptsTest extends TestCase
                     "State"    => "CA",
                     "Country"  => "USA",
                     "Zip"      => "60654",
-                    "Type"     => 2,
+                    "Type"     => EazyScripts::TYPE_WORK,
                 ],
                 "HomePhoneNumber" => [
-                    "Number"    => "0000000000",
+                    "Number"    => "4155552671",
                     "Extension" => "+1",
-                    "Type"      => 1,
+                    "Type"      => EazyScripts::TYPE_HOME,
                 ],
                 "WorkPhoneNumber" => [
-                    "Number"    => "0000000000",
+                    "Number"    => "4155552671",
                     "Extension" => "+1",
-                    "Type"      => 2,
+                    "Type"      => EazyScripts::TYPE_WORK,
                 ],
             ],
         ]);
 
-        $this->assertObjectNotHasAttribute('error', $response->getBody());
-        $this->assertObjectNotHasAttribute('errors', $response->getBody());
+        $this->assertObjectNotHasAttribute('error', (object)$response->getBody());
+        $this->assertObjectNotHasAttribute('errors', (object)$response->getBody());
 
         $this->assertObjectHasAttribute('id', $response->getBody());
 
@@ -127,8 +128,8 @@ final class EazyScriptsTest extends TestCase
 
         $response = $api->getPatients();
 
-        $this->assertObjectNotHasAttribute('error', $response->getBody());
-        $this->assertObjectNotHasAttribute('errors', $response->getBody());
+        $this->assertObjectNotHasAttribute('error', (object)$response->getBody());
+        $this->assertObjectNotHasAttribute('errors', (object)$response->getBody());
     }
 
     public function testCanGetPatient()
@@ -143,8 +144,8 @@ final class EazyScriptsTest extends TestCase
 
         $response = $api->getPatient(self::$patient_id);
 
-        $this->assertObjectNotHasAttribute('error', $response->getBody());
-        $this->assertObjectNotHasAttribute('errors', $response->getBody());
+        $this->assertObjectNotHasAttribute('error', (object)$response->getBody());
+        $this->assertObjectNotHasAttribute('errors', (object)$response->getBody());
     }
 
     public function testCanUpdatePatient()
@@ -161,8 +162,48 @@ final class EazyScriptsTest extends TestCase
             "consent" => null,
         ]);
 
-        $this->assertObjectNotHasAttribute('error', $response->getBody());
-        $this->assertObjectNotHasAttribute('errors', $response->getBody());
+        $this->assertObjectNotHasAttribute('error', (object)$response->getBody());
+        $this->assertObjectNotHasAttribute('errors', (object)$response->getBody());
+    }
+
+    public function testCanGetPrescriberSpecialties()
+    {
+        $api = new EazyScripts(
+            getenv('EAZYSCRIPTS_KEY'),
+            getenv('EAZYSCRIPTS_SECRET'),
+            getenv('EAZYSCRIPTS_SUBDOMAIN')
+        );
+
+        $api->setToken(self::$token);
+
+        $response = $api->getPrescriberSpecialties();
+
+        $this->assertObjectNotHasAttribute('error', (object)$response->getBody());
+        $this->assertObjectNotHasAttribute('errors', (object)$response->getBody());
+
+        $this->assertNotEmpty($response->getBody());
+
+        self::$specialty_id = $response->getBody()[0]->value;
+    }
+
+    public function testCanGetPrescriberSpecialtyQualifiers()
+    {
+        $api = new EazyScripts(
+            getenv('EAZYSCRIPTS_KEY'),
+            getenv('EAZYSCRIPTS_SECRET'),
+            getenv('EAZYSCRIPTS_SUBDOMAIN')
+        );
+
+        $api->setToken(self::$token);
+
+        $response = $api->getPrescriberSpecialtyQualifiers();
+
+        $this->assertObjectNotHasAttribute('error', (object)$response->getBody());
+        $this->assertObjectNotHasAttribute('errors', (object)$response->getBody());
+        
+        $this->assertNotEmpty($response->getBody());
+
+        self::$qualifier_id = $response->getBody()[0]->value;
     }
 
     public function testCanAddPrescriber()
@@ -176,10 +217,49 @@ final class EazyScriptsTest extends TestCase
         $api->setToken(self::$token);
 
         $response = $api->addPrescriber([
+            "FirstName"   => "Testing",
+            "LastName"    => "Doctor",
+            "Email"       => time() . "testing+doctor@testemail.com",
+            "Password"    => "pa55word",
+            "DateOfBirth" => "1970-3-1",
+            "Gender"      => EazyScripts::GENDER_MALE,
+            "Prescriber"  => [
+                "Npi"                => "1234567890",
+                "Specialty"          => self::$specialty_id,
+                "SpecialtyQualifier" => self::$qualifier_id,
+                "ClinicName"         => "Test Clinic",
+                "Address"            => [
+                    "Type"     => EazyScripts::TYPE_WORK,
+                    "Address1" => "555 Noah Way",
+                    "City"     => "San Diego",
+                    "State"    => "CA",
+                    "Country"  => "USA",
+                    "Zip"      => "92117",
+                ],
+                "Permissions" => [
+                    "NewRx"               => false,
+                    "Refill"              => false,
+                    "Change"              => false,
+                    "Cancel"              => false,
+                    "ControlledSubstance" => false,
+                ],
+                "PhoneNumbers" => [
+                    [
+                        "Number"    => "4155552671",
+                        "Extension" => "+1",
+                        "Type"      => EazyScripts::TYPE_WORK,
+                    ],
+                    [
+                        "Number"    => "4155552671",
+                        "Extension" => "+1",
+                        "Type"      => EazyScripts::TYPE_FAX,
+                    ]
+                ],
+            ],
         ]);
 
-        $this->assertObjectNotHasAttribute('error', $response->getBody());
-        $this->assertObjectNotHasAttribute('errors', $response->getBody());
+        $this->assertObjectNotHasAttribute('error', (object)$response->getBody());
+        $this->assertObjectNotHasAttribute('errors', (object)$response->getBody());
 
         $this->assertObjectHasAttribute('id', $response->getBody());
 
@@ -198,8 +278,8 @@ final class EazyScriptsTest extends TestCase
 
         $response = $api->getPrescribers();
 
-        $this->assertObjectNotHasAttribute('error', $response->getBody());
-        $this->assertObjectNotHasAttribute('errors', $response->getBody());
+        $this->assertObjectNotHasAttribute('error', (object)$response->getBody());
+        $this->assertObjectNotHasAttribute('errors', (object)$response->getBody());
     }
 
     public function testCanGetPrescriber()
@@ -214,8 +294,8 @@ final class EazyScriptsTest extends TestCase
 
         $response = $api->getPrescriber(self::$prescriber_id);
 
-        $this->assertObjectNotHasAttribute('error', $response->getBody());
-        $this->assertObjectNotHasAttribute('errors', $response->getBody());
+        $this->assertObjectNotHasAttribute('error', (object)$response->getBody());
+        $this->assertObjectNotHasAttribute('errors', (object)$response->getBody());
     }
 
     public function testCanUpdatePrescriber()
@@ -228,9 +308,13 @@ final class EazyScriptsTest extends TestCase
 
         $api->setToken(self::$token);
 
-        $response = $api->updatePrescriber(self::$prescriber_id, []);
+        $response = $api->updatePrescriber(self::$prescriber_id, [
+            "Npi" => "1234567890",
+            "Specialty" => self::$specialty_id,
+            "SpecialtyQualifier" => self::$qualifier_id,
+        ]);
 
-        $this->assertObjectNotHasAttribute('error', $response->getBody());
-        $this->assertObjectNotHasAttribute('errors', $response->getBody());
+        $this->assertObjectNotHasAttribute('error', (object)$response->getBody());
+        $this->assertObjectNotHasAttribute('errors', (object)$response->getBody());
     }
 }
